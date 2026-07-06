35a831dfb9682d35b5c8e961f3e8f6c24d2bedd0
nitheebandara-dot <nitheebandara@gmail.com>
2026-07-06 17:44:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(
     page_title="AI Agriculture Research Engine",
     page_icon="🌱",
     layout="centered"
-)
+
 
 # ඊට පස්සේ අනිත් ටික
 st.title("🌱 AI Agriculture Research Engine")
